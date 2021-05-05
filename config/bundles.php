02092436f5574eb79e3c264660760f1540f886b0
Alexey Miheev <miheev_aa@groupbwt.com>
2021-05-05 16:20:28 +0300

    add user entity

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
