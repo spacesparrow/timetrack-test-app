433981138120d05a0214094187411027fd3a6aee
Alexey Miheev <miheev_aa@groupbwt.com>
2021-05-13 11:33:25 +0300

    cover ExcelTastsExportService by unit tests, add base class for export services testing

--- tests/Unit/Service/Export/ExcelTasksExportServiceTest.php
+++ tests/Unit/Service/Export/ExcelTasksExportServiceTest.php
@@ -6,13 +6,14 @@ namespace App\Tests\Unit\Service\Export;
 
 use App\DTO\TasksExportDTO;
 use App\Exception\UnsupportedExportFormatException;
-use App\Service\Export\CsvTasksExportService;
+use App\Service\Export\ExcelTasksExportService;
+use PhpOffice\PhpSpreadsheet\Writer\Exception;
 use ReflectionException;
 
-class CsvTasksExportServiceTest extends GeneralTasksExportServiceTest
+class ExcelTasksExportServiceTest extends GeneralTasksExportServiceTest
 {
     /**
-     * @covers       \App\Service\Export\CsvTasksExportService::supports
+     * @covers       \App\Service\Export\ExcelTasksExportService::supports
      * @dataProvider dataProviderForSupportsTesting
      * @param TasksExportDTO $exportDTO
      * @param bool $expected
@@ -38,15 +39,16 @@ class CsvTasksExportServiceTest extends GeneralTasksExportServiceTest
 
         return [
             self::TYPE_PDF => [$pdfExport, false],
-            self::TYPE_CSV => [$csvExport, true],
-            self::TYPE_XLSX => [$xlsxExport, false],
+            self::TYPE_CSV => [$csvExport, false],
+            self::TYPE_XLSX => [$xlsxExport, true],
         ];
     }
 
     /**
-     * @covers \App\Service\Export\CsvTasksExportService::export
+     * @covers \App\Service\Export\ExcelTasksExportService::export
      * @throws ReflectionException
      * @throws UnsupportedExportFormatException
+     * @throws Exception
      */
     public function testExport(): void
     {
@@ -56,17 +58,17 @@ class CsvTasksExportServiceTest extends GeneralTasksExportServiceTest
 
         $exportResponseDTO = $this->getTestingService()->export($exportDTO);
 
-        static::assertSame('tasks_export.' . self::TYPE_CSV, $exportResponseDTO->getFilename());
+        static::assertSame('tasks_export.' . self::TYPE_XLSX, $exportResponseDTO->getFilename());
         static::assertStringContainsString($exportResponseDTO->getFilename(), $exportResponseDTO->getTempFile());
         static::assertStringContainsString('tmp', $exportResponseDTO->getTempFile());
     }
 
     /**
-     * @return CsvTasksExportService
+     * @return ExcelTasksExportService
      */
-    protected function getTestingService(): CsvTasksExportService
+    protected function getTestingService(): ExcelTasksExportService
     {
-        $service = new CsvTasksExportService();
+        $service = new ExcelTasksExportService();
         $service->setTranslator($this->translatorMock);
 
         return $service;
---
+++ tests/Unit/Service/Export/GeneralTasksExportServiceTest.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Service\Export;
+
+use App\DTO\TasksExportDTO;
+use App\Entity\Task;
+use App\Tests\Traits\ReflectionTrait;
+use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use ReflectionException;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+abstract class GeneralTasksExportServiceTest extends TestCase
+{
+    use ReflectionTrait;
+
+    /** @var string */
+    protected const TYPE_PDF = 'pdf';
+    /** @var string */
+    protected const TYPE_CSV = 'csv';
+    /** @var string */
+    protected const TYPE_XLSX = 'xlsx';
+
+    /** @var TranslatorInterface|MockObject */
+    protected $translatorMock;
+
+    /**
+     * @return TasksExportDTO
+     * @throws ReflectionException
+     */
+    protected function getTestingData(): TasksExportDTO
+    {
+        $previousTask = new Task();
+        $this->setNonPublicPropertyValue($previousTask, 'id', 5);
+        $previousTask->setTitle('Previous title');
+        $previousTask->setComment('Previous comment');
+        $previousTask->setTimeSpent(15);
+        $previousTask->setCreatedDate(new DateTime('yesterday'));
+
+        $currentTask = new Task();
+        $this->setNonPublicPropertyValue($currentTask, 'id', 6);
+        $currentTask->setTitle('Current title');
+        $currentTask->setComment('Current comment');
+        $currentTask->setTimeSpent(20);
+        $currentTask->setCreatedDate(new DateTime());
+
+        $exportDTO = new TasksExportDTO();
+        $exportDTO->setType(self::TYPE_CSV);
+        $exportDTO->setStartDate(new DateTime('2 days ago'));
+        $exportDTO->setEndDate(new DateTime());
+        $exportDTO->setTasks(new ArrayCollection([$previousTask, $currentTask]));
+
+        return $exportDTO;
+    }
+
+    protected function performExpectations(): void
+    {
+        $this->translatorMock->expects(static::once())
+            ->method('trans')
+            ->with(...['Total spent'])
+            ->willReturn('Total spent:');
+    }
+
+    abstract protected function getTestingService();
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->translatorMock = $this->createMock(TranslatorInterface::class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown(): void
+    {
+        unset($this->translatorMock);
+
+        parent::tearDown();
+    }
+}
